7d92d0e49e8245f5463b8714c8e6ede62d6846b8
Carlos Guerrero <carlos.guerrero@utec.edu.pe>
2019-06-03 09:49:23 -0500

    1 && 2

--- src/composing/extract-variable_before.java
+++ src/composing/extract-variable_before.java
@@ -1,7 +1,7 @@
 void renderBanner() {
-    const bool thereIsMAC = platform.toUpperCase().indexOf("MAC") > -1;
-    const bool thereIsIE = browser.toUpperCase().indexOf("IE") > -1;
-    const bool resizeExist = resize > 0;
+    final boolean thereIsMAC = platform.toUpperCase().indexOf("MAC") > -1;
+    final boolean thereIsIE = browser.toUpperCase().indexOf("IE") > -1;
+    final boolean resizeExist = resize > 0;
 
   if thereIsMac && thereIsIE && wasInicialized() && resizeExist) {
     // do something
