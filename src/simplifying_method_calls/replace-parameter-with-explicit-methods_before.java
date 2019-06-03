0bd2b37a5371b0901fb80142ffa4ca2f705f1890
Carlos Guerrero <carlos.guerrero@utec.edu.pe>
2019-06-03 10:28:08 -0500

    9

--- src/simplifying_method_calls/replace-parameter-with-explicit-methods_before.java
+++ src/simplifying_method_calls/replace-parameter-with-explicit-methods_before.java
@@ -1,11 +1,7 @@
-void setValue(String name, int value) {
-  if (name.equals("height")) {
-    height = value;
-    return;
-  }
-  if (name.equals("width")) {
-    width = value;
-    return;
-  }
-  Assert.shouldNeverReachHere();
+void setHeight(int newHeight) {
+    height = newHeight;
+}
+
+void setWidth(int newWidth) {
+    width = newWidth;
 }
