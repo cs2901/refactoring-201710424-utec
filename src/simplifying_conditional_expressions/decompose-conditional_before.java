30a79b7644acf78f678e6b010426683252310700
Carlos Guerrero <carlos.guerrero@utec.edu.pe>
2019-06-03 10:11:58 -0500

    6

--- src/simplifying_conditional_expressions/decompose-conditional_before.java
+++ src/simplifying_conditional_expressions/decompose-conditional_before.java
@@ -1,4 +1,7 @@
-if (date.before(SUMMER_START) || date.after(SUMMER_END)) {
+boolean isSummer(Date date) {
+    return date.after(SUMMER_START) && date.before(SUMMER_END);
+        }
+if (!isSummer(date)) {
   charge = quantity * winterRate + winterServiceCharge;
 }
 else {
